87be25709ed0d11252e68303723fb439fd0f6ec0
SzabolcsGergely <szabi@luxonis.com>
2021-05-19 02:24:18 +0300

    Update core; restart video writing after 5gb in rgb encoding example, only for stress stesting

--- examples/04_rgb_encoding.py
+++ examples/04_rgb_encoding.py
@@ -26,20 +26,25 @@ with dai.Device(pipeline) as device:
     # Output queue will be used to get the encoded data from the output defined above
     q = device.getOutputQueue(name="h264", maxSize=30, blocking=True)
 
-    # The .h264 file is a raw stream file (not playable yet)
-    with open('video.h264', 'wb') as videoFile:
-        print("Press Ctrl+C to stop encoding...")
-        try:
-            while True:
-                h264Packet = q.get()  # Blocking call, will wait until a new data has arrived
-                h264Data = h264Packet.getData()
-                h264Data.tofile(videoFile)  # Appends the packet data to the opened file
-                print(f"received {cnt} size {len(h264Data)}")
-                cnt+=1
-        except KeyboardInterrupt:
-            # Keyboard interrupt (Ctrl + C) detected
-            pass
+    try:
+        while True:
+            maxSize = 5 * 1024 * 1024 * 1024
+            totalSizeReceived = 0
+            # The .h264 file is a raw stream file (not playable yet)
+            with open('video.h264', 'wb') as videoFile:
+                print("Press Ctrl+C to stop encoding...")
 
+                while totalSizeReceived < maxSize:
+                    h264Packet = q.get()  # Blocking call, will wait until a new data has arrived
+                    h264Data = h264Packet.getData()
+                    h264Data.tofile(videoFile)  # Appends the packet data to the opened file
+                    print(f"received {cnt} size {len(h264Data)}")
+                    totalSizeReceived+=len(h264Data)
+                    cnt+=1
+
+    except KeyboardInterrupt:
+        # Keyboard interrupt (Ctrl + C) detected
+        pass
 
     print("To view the encoded data, convert the stream file (.h264) into a video file (.mp4) using a command below:")
     print("ffmpeg -framerate 30 -i video.h264 -c copy video.mp4")
